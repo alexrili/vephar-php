72111fa4d338879cc4e59e89417973a5ca3f85b1
Alex Ribeiro de Lima <alexrili@gmail.com>
2022-12-14 00:37:15 -0300

    add go deeper false support and to original array suport (#12)

---
+++ tests/Fake/CustomResourceCases.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Hell\Vephar\Fake;
+
+use Hell\Vephar\Resource;
+
+/**
+ * @author '@alexrili'
+ * @class CustomResourceCases
+ * @package Hell\Vephar\Fake
+ */
+class CustomResourceCases extends Resource
+{
+    /**
+     * @var
+     */
+    public $firstAttribute;
+
+}
